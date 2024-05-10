b72cfb872cd98b6aff39e218c62f9fcd132427d3
BettyKagendo <gitongabetty.k@gmail.com>
2024-05-10 14:21:49 +0300

    concatenate  elements of word_display

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import random
 #create a list of words
 hangman_words = ['whizzing', 'fishhook', 'beekeper', 'rhythm', 'stronghold']
 
-def get_random_word():
+#def get_random_word():
     #choose a random word from the list
-    word = random.choice(hangman_words)
-    return word
+word = random.choice(hangman_words)
+    #return word
 #print (get_random_word())
 
 
-def display_word (word):
+#def display_word (word):
     #create a list of dashes for each letter in the word to be guesssed
-    word_display = ['_'] * len(word)
-    return word_display
-    #print(' ,'.join(word_display)) #concatenates all the elements of word_display and separates them with spaces
+word_display = ['_'] * len(word)
+    #return word_display
+print(' '.join(word_display)) #concatenates all the elements of word_display and separates them with spaces
 
 
 
