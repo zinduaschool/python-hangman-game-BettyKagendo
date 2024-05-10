cc91a6b02dc2a7f88cf0bd2804faf858957fa5fe
BettyKagendo <gitongabetty.k@gmail.com>
2024-05-10 14:17:54 +0300

    define display_word function

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ def get_random_word():
     #choose a random word from the list
     word = random.choice(hangman_words)
     return word
-print (get_random_word())
+#print (get_random_word())
+
+
+def display_word (word):
+    #create a list of dashes for each letter in the word to be guesssed
+    word_display = ['_'] * len(word)
+    return word_display
+    #print(' ,'.join(word_display)) #concatenates all the elements of word_display and separates them with spaces
+
+
 
 
 
--- trial.py
+++ trial.py
@@ -9,10 +9,5 @@ used_letters = set()
 print (word_letters)
 
 
-def display_word (word):
-    #create a list of dashes for each letter in the word to be guesssed
-    word_display = ['_'] * len(word)
-    print(' ,'.join(word_display)) #concatenates all the elements of word_display and separates them with spaces
-
 #main game loop
 def play_hangman():
